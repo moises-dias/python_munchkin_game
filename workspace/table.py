3abb2e029691bc9687efd8f3cc0bc428ba05ae01
moises.dias <moises.dias@dextra-sw.com>
2021-09-04 12:26:32 -0300

    fixing table sizes

--- workspace/table.py
+++ workspace/table.py
@@ -1,8 +1,8 @@
 import pygame
 from field import Field
 
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
+SCREEN_WIDTH = 1266
+SCREEN_HEIGHT = 668
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -12,13 +12,16 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 PURPLE = (255, 0, 255)
 
+x_limits = [0.4, 0.8]
+y_limits = [0.25, 0.75]
+
 rects = []
-rects.append(Field(0, 0, int(0.4*SCREEN_WIDTH), int(0.3*SCREEN_HEIGHT), RED, 'players'))
-rects.append(Field(0, int(0.3*SCREEN_HEIGHT), int(0.4*SCREEN_WIDTH), int(0.8*SCREEN_HEIGHT), BLACK, 'equipments'))
-rects.append(Field(0, int(0.8*SCREEN_HEIGHT), int(0.4*SCREEN_WIDTH), SCREEN_HEIGHT, GREY, 'hand'))
-rects.append(Field(int(0.4*SCREEN_WIDTH), 0, SCREEN_WIDTH, int(0.8*SCREEN_HEIGHT), GREEN, 'table'))
-rects.append(Field(int(0.4*SCREEN_WIDTH), int(0.8*SCREEN_HEIGHT), int(0.7*SCREEN_WIDTH), SCREEN_HEIGHT, BLUE, 'deck'))
-rects.append(Field(int(0.7*SCREEN_WIDTH), int(0.8*SCREEN_HEIGHT), SCREEN_WIDTH, SCREEN_HEIGHT, PURPLE, 'logs'))
+rects.append(Field(0, 0, int(x_limits[0] * SCREEN_WIDTH), int(y_limits[0] * SCREEN_HEIGHT), RED, 'players'))
+rects.append(Field(0, int(y_limits[0] * SCREEN_HEIGHT), int(x_limits[0] * SCREEN_WIDTH), int(y_limits[1] * SCREEN_HEIGHT), BLACK, 'equipments'))
+rects.append(Field(0, int(y_limits[1] * SCREEN_HEIGHT), int(x_limits[0] * SCREEN_WIDTH), SCREEN_HEIGHT, GREY, 'hand'))
+rects.append(Field(int(x_limits[0] * SCREEN_WIDTH), 0, SCREEN_WIDTH, int(y_limits[1] * SCREEN_HEIGHT), GREEN, 'table'))
+rects.append(Field(int(x_limits[0] * SCREEN_WIDTH), int(y_limits[1] * SCREEN_HEIGHT), int(x_limits[1] * SCREEN_WIDTH), SCREEN_HEIGHT, BLUE, 'deck'))
+rects.append(Field(int(x_limits[1] * SCREEN_WIDTH), int(y_limits[1] * SCREEN_HEIGHT), SCREEN_WIDTH, SCREEN_HEIGHT, PURPLE, 'logs'))
 
 FPS = 30
 
