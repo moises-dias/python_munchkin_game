c04843dda021b726a38a2fee2161c683e5c1c4aa
moises.dias <moises.dias@dextra-sw.com>
2021-09-25 12:48:49 -0300

    handling special characters on player name

--- client_server/client.py
+++ client_server/client.py
@@ -2,11 +2,15 @@ from network import Network
 from card_deck.game import play
 from card_deck.game import listen
 from threading import Thread
+import re
 
 def main():
-    with open('client_server/conf.txt') as f:
+    with open('client_server/conf.txt', encoding="utf8") as f:
         lines = [line.strip() for line in f.readlines()]
-        name = lines[0][:6].lower()
+        name = lines[0]
+        name = re.sub('[^A-Za-z]+', '', name)
+        name = name.lower()
+        name = name[:6]
         ip = lines[1]
     network = Network(name, ip)
     #play(network)
